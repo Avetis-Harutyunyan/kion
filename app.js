bb667fdce81176c9a9c0713cee7a40bcc4908e8d
AvetisHarutyunyan <87875341+Avetis-Harutyunyan@users.noreply.github.com>
2021-12-17 17:36:56 +0400

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,15 @@
 function navSide() {
     const bars = document.querySelector("#burger");
     const nav = document.querySelector("#navSide");
+    const bodyBottom = document.querySelector("body");
 
-    bars.addEventListener("click", function() {
+    bars.addEventListener("click", function () {
         nav.classList.toggle("nav-active");
 
         // burger animation
         bars.classList.toggle("close");
+
+        bodyBottom.classList.toggle("body_bottom");
     });
 }
 navSide();
